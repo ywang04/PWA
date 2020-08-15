383a23c169943c9b62756e280b57fbcc3289ef67
Yang Wang <yang.wang@optus.com.au>
2020-08-15 19:35:15 +1000

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function App(props) {
+  const [count, setCount] = useState(() => {
+    console.log('testing');
+    return props.defaultCount || 0;
+  });
+
+  const handleClick = () => {
+    setCount(c => c + 1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleClick}>Count {count}</button>
     </div>
   );
 }
