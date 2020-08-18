5960d711b4c7a14c569eafb488e86fc479e3a42a
Yang Wang <yang.wang@optus.com.au>
2020-08-18 10:07:15 +1000

    useState

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 
 function App(props) {
+  // state的默认值基于props
+  // 函数只在第一次渲染时被调用 函数的返回值就是默认值
   const [count, setCount] = useState(() => {
     console.log('testing');
     return props.defaultCount || 0;
