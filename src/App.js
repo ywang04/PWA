ca3ff29ea13f3cb2c08824dc93371ca3728cb79e
Yang Wang <yang.wang@optus.com.au>
2020-08-19 20:25:06 +1000

    useCallback

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useCallback, memo } from 'react';
 
-const Counter = ({ count }) => <h1>{count}</h1>;
+const Counter = memo(({ handleClick }) => {
+  console.log('i m rendering')
+  return (<h1 onClick={handleClick}>test</h1>);
+});
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,10 +11,21 @@ function App() {
 
   const half = useMemo(() => double / 4, [double]);
 
+  //useMemo返回值是函数的话 就相当于useCallback
+  // useMemo(() => fn) useCallback(fn)
+  const handleClick = useMemo(() => {
+    console.log(123); //只会运行一次 注意函数被创建和函数被调用的区别
+    return () => {
+      setCount(c => c + 1);
+    };
+  }, []);
+
   return (
     <div className="App">
-      <button onClick={() => setCount(c => c + 1)}>Click:{count}, double:{double}, half:{half} </button>
-      <Counter count={count} />
+      <button onClick={() => setCount(c => c + 1)}>
+        Click:{count}, double:{double}, half:{half}{' '}
+      </button>
+      <Counter handleClick={handleClick} />
     </div>
   );
 }
